af83c14ddb638eec5b347342262beab64ccc779c
Ruslan <krasnovrg@gmail.com>
2022-03-04 18:24:34 +0500

    HW2 cloudstorage
    Added all commands

--- src/main/java/com/geekbrains/cloud/nio/EchoServerNio.java
+++ src/main/java/com/geekbrains/cloud/nio/EchoServerNio.java
@@ -1,5 +1,6 @@
 package com.geekbrains.cloud.nio;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -8,16 +9,21 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
-import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class EchoServerNio {
 
     private ServerSocketChannel serverSocketChannel;
     private Selector selector;
     private ByteBuffer buf;
+    private Path currentDirectory;
 
     public EchoServerNio() throws Exception {
+        currentDirectory = Paths.get("server");
         buf = ByteBuffer.allocate(5);
         serverSocketChannel = ServerSocketChannel.open();
         selector = Selector.open();
@@ -74,9 +80,41 @@ public class EchoServerNio {
             buf.clear();
         }
 
-        String msg = "From server: " + reader.toString();
+        String msg = reader.toString().trim();
+        //added
+        if ("ls".equals(msg)) {
+            channel.write(ByteBuffer.wrap(getDirFiles(currentDirectory).getBytes(StandardCharsets.UTF_8)));
+            printArrow(channel);
+        } else if (msg.startsWith("cd ")) {
+            Path newPath = Paths.get(msg.substring(3, msg.length()));
+            if (Files.exists(newPath)) {
+                currentDirectory = newPath;
+                System.out.println("Current directory changed!");
+                channel.write(ByteBuffer.wrap("Current directory changed!\n\r".getBytes(StandardCharsets.UTF_8)));
+                printArrow(channel);
+            }
+        } else if(msg.startsWith("cat ") && msg.endsWith(".txt")){
+            if (Files.exists(Paths.get(currentDirectory.toString() + "\\" + msg.substring(4, msg.length())))) {
+                Path fileToOpen = currentDirectory.resolve(msg.substring(4, msg.length()));
+                byte[] bytes = Files.readAllBytes(fileToOpen);
+                channel.write(ByteBuffer.wrap(bytes));
+                channel.write(ByteBuffer.wrap("\n\r".getBytes(StandardCharsets.UTF_8)));
+                printArrow(channel);
+            } else {
+                channel.write(ByteBuffer.wrap("No such file in current directory!!!\n\r".getBytes(StandardCharsets.UTF_8)));
+                printArrow(channel);
+            }
+        } else if(msg.startsWith("mkdir ") && (msg.length() > 6)) {
+            Files.createDirectory(Paths.get(currentDirectory + "\\" + msg.substring(6, msg.length())));
+            printArrow(channel);
+        } else if(msg.startsWith("touch ") && (msg.length() > 6)) {
+            Files.createFile(Paths.get(currentDirectory + "\\" + msg.substring(6, msg.length())));
+            printArrow(channel);
+        }
+        else {
+            printArrow(channel);
+        }
         System.out.println("Received: " + msg);
-        channel.write(ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8)));
     }
 
     private void handleAccept() throws IOException {
@@ -84,6 +122,19 @@ public class EchoServerNio {
         socketChannel.configureBlocking(false);
         socketChannel.register(selector, SelectionKey.OP_READ);
         System.out.println("Client accepted...");
+        //added
+        socketChannel.write(ByteBuffer.wrap("Welcome to terminal\n\r".getBytes(StandardCharsets.UTF_8)));
+        printArrow(socketChannel);
+    }
+
+    private String getDirFiles(Path path) throws IOException {
+        return Files.list(path)
+                .map(p -> p.getFileName().toString())
+                .collect(Collectors.joining("\n\r")) + "\n\r";
+    }
+
+    private void printArrow(SocketChannel socketChannel) throws IOException {
+        socketChannel.write(ByteBuffer.wrap("-> ".getBytes(StandardCharsets.UTF_8)));
     }
 
     public static void main(String[] args) throws Exception {
